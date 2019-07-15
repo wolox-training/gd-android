d550f2b79510b062f034ee6f2bf67bb19502ddcf
Gabriel Diaz <gabriel.diaz@wolox.com.ar>
2019-07-15 12:38:56 -0300

    fixes

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/news/NewsAdapterCallBack.kt
@@ -0,0 +1,13 @@
+package ar.com.wolox.android.example.ui.news
+
+import androidx.recyclerview.widget.DiffUtil
+import ar.com.wolox.android.example.model.News
+
+class NewsAdapterCallBack : DiffUtil.ItemCallback<News>() {
+    override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
+        return oldItem.id == newItem.id
+    }
+    override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
+        return (oldItem.title == newItem.title && oldItem.text == newItem.text)
+    }
+}
--- app/src/main/java/ar/com/wolox/android/example/ui/news/NewsViewHolderAdapter.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/news/NewsViewHolderAdapter.kt
@@ -62,12 +62,3 @@ class NewsViewHolderAdapter : ListAdapter<News, NewsViewHolderAdapter.ViewHolder
         var vFavButton = item.findViewById<ToggleButton>(R.id.vFavButton)
     }
 }
-
-class NewsAdapterCallBack : DiffUtil.ItemCallback<News>() {
-    override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
-        return oldItem.id == newItem.id
-    }
-    override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
-        return (oldItem.title == newItem.title && oldItem.text == newItem.text)
-    }
-}
