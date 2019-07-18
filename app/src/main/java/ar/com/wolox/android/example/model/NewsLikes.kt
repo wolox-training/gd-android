e59a182c676796f1110677a21bfc0e8be636e9e1
Gabriel Diaz <gabriel.diaz@wolox.com.ar>
2019-07-18 11:29:02 -0300

    fixes

--- app/src/main/java/ar/com/wolox/android/example/model/NewsLikes.kt
+++ app/src/main/java/ar/com/wolox/android/example/model/NewsLikes.kt
@@ -3,6 +3,5 @@ package ar.com.wolox.android.example.model
 import com.google.gson.annotations.SerializedName
 
 class NewsLikes(
-    @SerializedName("likes")
-    val likes: List<Int>? = null
+    @SerializedName("likes") val likes: List<Int>? = null
 )
